5ceba2a12fa7c0c24663add69d78cf93545dc209
gandrew1985 <gandrew@op.pl>
2019-12-15 19:26:04 +0000

    testy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,26 +5,5 @@ import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args) {
-
-        SimpleUser simpleUser = new SimpleUser("theForumuser");
-
-        String result = simpleUser.getUserName();
-
-        if(result.equals("theForumuser")) {
-            System.out.println("Test ok");
-        } else {
-            System.out.println("Error");
-        }
-        //Calculator test:
-        Calculator calculator = new Calculator();
-
-        int resultAdd = calculator.add(10, 5);
-        int resultSub = calculator.subtract(7,2);
-
-        if((resultAdd == 15) && (resultSub == 5)) {
-            System.out.println("Calculator Test OK!");
-        } else {
-            System.out.println("Calculator test wrong");
-        }
     }
 }
