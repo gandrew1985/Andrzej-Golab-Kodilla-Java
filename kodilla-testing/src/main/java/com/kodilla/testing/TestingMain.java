6ab35aae3a5a93f07b7a016083cce400c052c9cf
gandrew1985 <gandrew@op.pl>
2019-12-16 09:43:24 +0000

    Calculator Test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,5 +5,15 @@ import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+       int resultAdd = calculator.add(10,5);
+       int resultSub = calculator.subtract(7,2);
+
+       if((resultAdd == 15) && (resultSub == 5)) {
+           System.out.println("Test calculator ok!");
+        } else {
+           System.out.println("Calculator error");
+        }
     }
 }
