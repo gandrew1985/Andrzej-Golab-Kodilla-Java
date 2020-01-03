cdff9d386dca43056524514372a8055a3deed59d
gandrew1985 <gandrew@op.pl>
2020-01-03 13:40:57 +0000

    Zad 7.3 corr

--- kodilla-stream/src/main/java/StreamMain.java
+++ kodilla-stream/src/main/java/StreamMain.java
@@ -12,37 +12,14 @@ public class StreamMain {
 
         Forum theForum = new Forum();
 
-        List<ForumUser> resultOfMenList = theForum.getUserList().stream()
-                .filter(forumuser -> forumuser.getUserSex() == 'M')
-                .collect(Collectors.toList());
-
-        System.out.println("List of Male ForumUser contain #elements: " + resultOfMenList.size());
-        resultOfMenList.stream()
-                .forEach(System.out::println);
-
-        List<ForumUser> resultListOf20YearsOldUsers = theForum.getUserList().stream()
-                .filter(forumuser -> forumuser.getDateOfBirth().getYear() <= 2000)
-                .collect(Collectors.toList());
-
-        System.out.println("\nList of 20 years old users contain #elements: "
-                + resultListOf20YearsOldUsers.size());
-        resultListOf20YearsOldUsers.stream()
-                .forEach(System.out::println);
-
-        List<ForumUser> resultListOfPostPosted = theForum.getUserList().stream()
-                .filter(forumuser -> forumuser.getNumberOfPostedPosts() >= 1)
-                .collect(Collectors.toList());
-
-        System.out.println("\nList of Users posted at least one post contain # elements: "
-                + resultListOfPostPosted.size());
-        resultListOfPostPosted.stream()
-                .forEach(System.out::println);
-
-        Map<Integer, ForumUser> resultOfMapForum = theForum.getUserList().stream()
+        Map<Integer, ForumUser> resultOfForumMap = theForum.getUserList().stream()
+                .filter(forumuser -> forumuser.getUserSex() == 'M' &&
+                        forumuser.getDateOfBirth().getYear() <= 2000 &&
+                        forumuser.getNumberOfPostedPosts() >= 1)
                 .collect(Collectors.toMap(ForumUser::getUniqueUserId, forumuser -> forumuser));
 
-        System.out.println("\nThe forum map contains #elements: " + resultOfMapForum.size());
-        resultOfMapForum.entrySet().stream()
+        System.out.println("\nThe forum map contains #elements: " + resultOfForumMap.size());
+        resultOfForumMap.entrySet().stream()
                 .map(entry -> entry.getKey() + ": " + entry.getValue())
                 .forEach(System.out::println);
     }
