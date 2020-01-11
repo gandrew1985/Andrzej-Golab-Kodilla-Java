d3bac38a4556b254cd983c6e787bbca97d938e88
gandrew1985 <gandrew@op.pl>
2020-01-11 11:07:03 +0000

    Zad 8.2 corr

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -3,30 +3,32 @@ package com.kodilla.exception.test;
 public class FirstChallenge {
 
     public double divide(double a, double b) throws ArithmeticException {
-        try {
-            if (b == 0) {
-                throw new ArithmeticException("Cant divide by 0");
-            }
-        } catch (ArithmeticException e) {
-            System.out.println("Something went wrong!! Error: " + e);
-        } finally {
-            System.out.println("Try is finished");
+        if(b == 0){
+            throw new ArithmeticException();
         }
-        return a/b;
+        return a / b;
     }
 
     /**
      * This main can throw an ArithmeticException!!!
-     *
      * @param args
      */
     public static void main(String[] args) {
 
         FirstChallenge firstChallenge = new FirstChallenge();
 
-        double result = firstChallenge.divide(3, 0);
+        try {
 
-        System.out.println(result);
+            double result = firstChallenge.divide(3, 0);
+
+        } catch (ArithmeticException e) {
+
+            System.out.println("Cant divide by 0!! Error: " + e);
+
+        } finally {
+
+            System.out.println("Finish");
+        }
 
     }
 }
