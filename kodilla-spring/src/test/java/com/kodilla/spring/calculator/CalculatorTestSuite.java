dc5ad274f5c584fab771c0eeb553271c58cb7c15
gandrew1985 <gandrew@op.pl>
2020-01-20 23:24:15 +0000

    zad 10.2

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,31 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalculatorTestSuite {
+
+    @Test
+    public void testCalculations() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double add = calculator.add(10.0,5.0);
+        double sub = calculator.sub(10.0,5.0);
+        double mul = calculator.mul(10.0,5.0);
+        double div = calculator.div(10.0,5.0);
+        //Then
+        //do nothing
+    }
+
+}
