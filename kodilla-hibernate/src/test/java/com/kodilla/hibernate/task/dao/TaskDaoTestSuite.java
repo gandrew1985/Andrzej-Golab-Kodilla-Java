dda8619705e854774937f84781fd3e265a401387
gandrew1985 <gandrew@op.pl>
2020-02-11 11:44:58 +0000

    mod 13.3

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
@@ -1,17 +1,19 @@
 package com.kodilla.hibernate.task.dao;
 
 import com.kodilla.hibernate.task.Task;
+import com.kodilla.hibernate.task.TaskFinancialDetails;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -47,6 +49,23 @@ public class TaskDaoTestSuite {
         assertEquals(1, readTasks.size());
         //CleanUp
         int id = readTasks.get(0).getId();
+        taskDao.deleteById(id);
+    }
+
+    @Test
+    public void testTaskDaoSaveWithFinancialDetails() {
+        //Given
+        Task task = new Task(DESCRIPTION, 30);
+        task.setTaskFinancialDetails(new TaskFinancialDetails(new BigDecimal(120), false));
+
+        //When
+        taskDao.save(task);
+        int id = task.getId();
+
+        //Then
+        assertNotEquals(0, id);
+
+        //CleanUp
         taskDao.deleteById(id);
     }
 }
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -1,5 +1,7 @@
 package com.kodilla.hibernate.tasklist.dao;
 
+import com.kodilla.hibernate.task.Task;
+import com.kodilla.hibernate.task.TaskFinancialDetails;
 import com.kodilla.hibernate.tasklist.TaskList;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,9 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -32,5 +36,28 @@ public class TaskListDaoTestSuite {
         //CleanUp
         int id = readLists.get(0).getId();
         taskListDao.deleteById(id);
+    }
+
+    @Test
+    public void testTaskListDaoSaveWithTasks() {
+        //Given
+        Task task = new Task("Test: do something", 14);
+        Task task2 = new Task("Write some entities", 3);
+        TaskFinancialDetails tdf = new TaskFinancialDetails(new BigDecimal(20),false);
+        TaskFinancialDetails tdf2 = new TaskFinancialDetails(new BigDecimal(10),false);
+        task.setTaskFinancialDetails(tdf);
+        task2.setTaskFinancialDetails(tdf2);
+        TaskList taskList = new TaskList(LIST_TO_DO,"ToDo tasks");
+        taskList.getTasks().add(task);
+        taskList.getTasks().add(task2);
+        task.setTaskList(taskList);
+        task2.setTaskList(taskList);
+        //When
+        taskListDao.save(taskList);
+        int id = taskList.getId();
+        //Then
+        assertNotEquals(0,id);
+        //CleanUp
+        //taskListDao.deleteById(id);
     }
 }
