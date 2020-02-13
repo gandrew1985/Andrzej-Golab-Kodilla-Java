0848bdad925bd9ebf8d4b4e46c28f73b9aea80a6
gandrew1985 <gandrew@op.pl>
2020-02-13 13:17:54 +0000

    zad13.4

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
@@ -5,6 +5,12 @@ import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
+@NamedNativeQuery(
+        name = "Company.findCompanyByLetters",
+        query = "SELECT * FROM COMPANIES " +
+                "WHERE COMPANY_NAME LIKE :name% ",
+        resultClass = Company.class
+)
 @Entity
 @Table(name = "COMPANIES")
 public class Company {
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -8,14 +8,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import javax.transaction.Transactional;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
 public class CompanyDaoTestSuite {
 
     @Autowired
     private CompanyDao companyDao;
+    @Autowired
+    private EmployeeDao employeeDao;
 
     @Test
     public void testSaveManyToMany() {
@@ -49,17 +56,54 @@ public class CompanyDaoTestSuite {
         int greyMatterId = greyMatter.getId();
 
         //Then
-        assertNotEquals(0,softwareMachineId);
-        assertNotEquals(0,dataMastersId);
-        assertNotEquals(0,greyMatterId);
+        assertNotEquals(0, softwareMachineId);
+        assertNotEquals(0, dataMastersId);
+        assertNotEquals(0, greyMatterId);
+    }
 
-        //CleanUp
-        //try {
-          //  companyDao.deleteById(softwareMachineId);
-            //companyDao.deleteById(dataMastersId);
-            //companyDao.deleteById(greyMatterId);
-        //} catch (Exception e) {
-            //do nothing
-        //}
+    @Test
+    public void testFindEmployeeByLastName() {
+        //Given
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+        Company softwareMachine = new Company("Software Machine");
+
+        softwareMachine.getEmployees().add(johnSmith);
+        softwareMachine.getEmployees().add(stephanieClarckson);
+        softwareMachine.getEmployees().add(lindaKovalsky);
+
+        companyDao.save(softwareMachine);
+        int softwareMachineId = softwareMachine.getId();
+
+        //When
+        List<Employee> findByLastName = employeeDao.findEmployeeByLastName("Kovalsky");
+
+        //Then
+        assertEquals(1, findByLastName.size());
+    }
+
+    @Test
+    public void testFindCompanyByLetters() {
+        //Given
+        Company softwareMachine = new Company("Software Machine");
+        Company dataMasters = new Company("Data Masters");
+        Company greyMatter = new Company("Grey Matter");
+
+        Employee johnSmith = new Employee("John", "Smith");
+
+        johnSmith.getCompanies().add(softwareMachine);
+        johnSmith.getCompanies().add(dataMasters);
+        johnSmith.getCompanies().add(greyMatter);
+
+        employeeDao.save(johnSmith);
+        int id = johnSmith.getId();
+
+        //When
+        List<Company> companies = companyDao.findCompanyByLetters("Sof%");
+
+        //Then
+        assertEquals(1, companies.size());
     }
 }
